edccf11156433a727fb5970a697ba12636bee1a2
stanographer <sakai.stanley@gmail.com>
2018-06-19 14:16:36 -0400

    First commit.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	"rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": 0,
+        "no-console": 0
+	}
+};
